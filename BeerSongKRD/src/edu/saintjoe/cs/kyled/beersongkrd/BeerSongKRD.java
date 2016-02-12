9dcc00919b12ac80f36f77d00bc5517c89b18564
kyled <kyled@localhost>
2016-02-12 08:29:09 -0500

    Fixed the "bottles" bug the way Capouch did it

--- BeerSongKRD/src/edu/saintjoe/cs/kyled/beersongkrd/BeerSongKRD.java
+++ BeerSongKRD/src/edu/saintjoe/cs/kyled/beersongkrd/BeerSongKRD.java
@@ -7,11 +7,6 @@ public class BeerSongKRD {
 		String word = "bottles";
 		
 		while (beerNum > 0) {
-			
-			if (beerNum == 1) {
-				word = "bottle";
-			}
-			
 			System.out.println(beerNum + " " + word + " of beer on the wall");
 			System.out.println(beerNum + " " + word + " of beer");
 			System.out.println("Take one down.");
